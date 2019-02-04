1befdc0a4224903118d5563861af5b98b337e2d3
n3mszn <n3mszn@gmail.com>
2019-02-04 22:07:03 +0100

    Navigation JS improvement

---
+++ js/game.js
@@ -0,0 +1,9 @@
+
+function renderDogTheCatcherGame() {
+    const gameContainer = document.querySelector('#game');
+    gameContainer.classList.remove('hidden');
+    gameContainer.classList.add('game-container-show');
+
+    scrollIt(gameContainer);
+}
+
