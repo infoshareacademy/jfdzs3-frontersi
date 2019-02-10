e401e02272a7afa27a507f289244abac2d4d8d2d
sooonk <sonia@przenioslo.eu>
2019-02-10 09:49:23 +0100

    Code refactoring

--- js/emailValidation.js
+++ js/emailValidation.js
@@ -1,29 +1,25 @@
-// EMAIL VALIDATION + GAME STARTER
-const submitBtn = document.querySelector('.submit-btn');
+const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
+const submitBtn = document.querySelector('.submit-btn');
+const emailInput = document.querySelector('.subscribe-input');
 const validationAlert = document.createElement('div');
-validationAlert.classList.add('alert');
-validationAlert.classList.add('my-red-alert');
-validationAlert.classList.add('alert-danger');
+const formLead = document.querySelector('.form-info');
+
+validationAlert.classList.add('alert', 'my-red-alert', 'alert-danger');
 validationAlert.innerText = 'Proszę wprowadzić prawidłowy adres email!';
 
 
-
 submitBtn.addEventListener('click', () => {
-    event.preventDefault();
-
-    const emailInput = document.querySelector('.subscribe-input');
-    const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (!filter.test(emailInput.value)) {
-        const formLead = document.querySelector('.form-info');
         formLead.appendChild(validationAlert);
         setTimeout(function () {
             validationAlert.parentNode.removeChild(validationAlert);
         }, 3000);
         emailInput.focus;
-        return false;
 
-    } else(
-        renderDogTheCatcherGame());
+        return false;
+    } else {
+        renderDogTheCatcherGame();
+    }
 });
