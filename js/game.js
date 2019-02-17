5d8a2eafb38be44ec197a72e1b1408b6fb26e46e
RadoslawM <radoslawmazurr@gmail.com>
2019-02-17 13:10:21 +0100

    add button instruction to game container

--- js/game.js
+++ js/game.js
@@ -5,3 +5,9 @@ function renderDogTheCatcherGame() {
 
     scrollElement(gameContainer);
 }
+
+$(document).ready(function(){
+    $("#instructionBtn").click(function(){
+      $("#myModal").modal();
+    });
+  });
